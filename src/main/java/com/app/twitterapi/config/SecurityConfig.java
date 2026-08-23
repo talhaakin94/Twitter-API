a691ce30a409a07d3449e3775faefe624e66b925
Talha <talhaakin094@gmail.com>
2026-08-24 01:20:55 +0300

    update

--- src/main/java/com/app/twitterapi/config/SecurityConfig.java
+++ src/main/java/com/app/twitterapi/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3200", "https://twitter-flame-three.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
